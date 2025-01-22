6c4d32178dd9af0ab1975801e225fc5c5a99ef68
muzafarova <alisapsee@gmail.com>
2025-01-22 18:47:16 +0000

    Single root element for the apod/:date page

--- pages/apod/[date].vue
+++ pages/apod/[date].vue
@@ -4,29 +4,31 @@ const apodMediaByDate = useApodMediaByDateStore();
 </script>
 
 <template>
-  <Title>{{ apodMediaByDate.pageTitle }}</Title>
+  <div>
+    <Title>{{ apodMediaByDate.pageTitle }}</Title>
 
-  <div class="p-4">
-    <div v-if="apodMediaByDate.status === 'pending'">Loading...</div>
-    <div v-if="apodMediaByDate.error">{{ apodMediaByDate.error }}</div>
+    <div class="p-4">
+      <div v-if="apodMediaByDate.status === 'pending'">Loading...</div>
+      <div v-if="apodMediaByDate.error">{{ apodMediaByDate.error }}</div>
 
-    <ApodMediaCard
-      v-if="apodMediaByDate.media"
-      :media-type="apodMediaByDate.media.media_type"
-      :url="
-        apodMediaByDate.media.media_type === 'image'
-          ? apodMediaByDate.media.hdurl
-          : apodMediaByDate.media.url
-      "
-      :title="apodMediaByDate.media.title"
-      :date="apodMediaByDate.media.date"
-      :description="apodMediaByDate.media.explanation"
-    >
-      <FavToggleButton
+      <ApodMediaCard
         v-if="apodMediaByDate.media"
-        :item="apodMediaByDate.media"
-        class="inline-flex"
-      />
-    </ApodMediaCard>
+        :media-type="apodMediaByDate.media.media_type"
+        :url="
+          apodMediaByDate.media.media_type === 'image'
+            ? apodMediaByDate.media.hdurl
+            : apodMediaByDate.media.url
+        "
+        :title="apodMediaByDate.media.title"
+        :date="apodMediaByDate.media.date"
+        :description="apodMediaByDate.media.explanation"
+      >
+        <FavToggleButton
+          v-if="apodMediaByDate.media"
+          :item="apodMediaByDate.media"
+          class="inline-flex"
+        />
+      </ApodMediaCard>
+    </div>
   </div>
 </template>
